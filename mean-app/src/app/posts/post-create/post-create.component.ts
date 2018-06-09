9fb6e3377409a1bfbbb22dbca719cd73af05f394
Jerzy Drobot <jerzydrobot@gmail.com>
2018-06-09 21:22:56 +0200

    Template driven forms

--- mean-app/src/app/posts/post-create/post-create.component.ts
+++ mean-app/src/app/posts/post-create/post-create.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Post } from '../post.mode';
 
 @Component({
   selector: 'app-post-create',
@@ -7,15 +9,17 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 export class PostCreateComponent {
 
-  enteredTitle = '';
-  enteredContent = '';
-  @Output() postCreated = new EventEmitter();
-
-  onAddPost() {
-    const post = {title: this.enteredTitle, content: this.enteredContent };
-    this.postCreated.emit(post);
+  @Output() postCreated = new EventEmitter<Post>();
+  
+  onAddPost(form: NgForm) {
+    if(form.invalid) return;
+    // Przypisanie pobranych wartości do obiektu post
+    const post: Post = {title: form.value.title, content: form.value.content }; 
+    this.postCreated.emit(post); // Wysłanie eventu z obiektem post -> $event zawiera teraz wartość post
   }
-
+  
+ // enteredTitle = ''; -> Zmienna pobiera wartość przez ngModel
+ // enteredContent = '';
   // onAddPost(postInput: HTMLTextAreaElement) {
     // this.newPost = postInput.value; -> Pobieranie wartości z referencji do inputa (Sposób pierwszy)
     // this.newPost = this.enteredValue; // Przypisanie wartości przez ngMNodel
